c9c0cc5ca006f6be6ee98b9893e45ea1e773c36e
Pushparaj Jayaseelan <pushparaj_jayaseelan@condenast.com>
2025-12-28 13:24:53 +0530

    sessionization based on user events done.

--- advanced-dataframe-sql-operations/session_metrics.py
+++ advanced-dataframe-sql-operations/session_metrics.py
@@ -1,11 +1,21 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
-from pyspark.sql.functions import col, lag, unix_timestamp,when, sum as spark_sum, min, max,count, countDistinct
+from pyspark.sql.functions import col, lag, unix_timestamp,when, sum as spark_sum, min, max,count, countDistinct, to_date, from_unixtime, concat_ws
 import time
+import os
+
+# Command to execute
+# spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.1,org.apache.hadoop:hadoop-aws:3.4.1,com.amazonaws:aws-java-sdk-bundle:1.12.262,mysql:mysql-connector-java:8.0.33 session_metrics.py
 
 spark = SparkSession.builder \
     .appName("Window Functions") \
     .master("local[*]") \
+    .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY")) \
+    .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_KEY")) \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
+    .config("spark.hadoop.fs.s3a.endpoint.region", os.getenv("AWS_DEFAULT_REGION")) \
+    .config('spark.jars.packages', "org.apache.hadoop:hadoop-aws:3.4.1,com.amazonaws:aws-java-sdk-bundle:1.11.1026,org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.1,mysql:mysql-connector-java:8.0.33") \
     .getOrCreate()
 
 data = [
@@ -41,28 +51,51 @@ df = df.withColumn("session_id", spark_sum("new_session").over(window))
 
 df.show()
 
-# Calculate session_start, session_end, total_events, session_duration, 
+# Calculate session_start, session_end, total_eventss, session_duration, 
 # is_bounce
 
 session_df = df.groupBy("user_id", "session_id") \
     .agg(
-        min("event_time").alias("s_start"),
-        max("event_time").alias("s_end"),
-        count("*").alias("total_event"),
-        countDistinct("event_type").alias("unique_events")
+        min("event_time").alias("session_start"),
+        max("event_time").alias("session_end"),
+        count("*").alias("total_events"),
+        countDistinct("event_type").alias("unique_event_types")
     ) \
-    .withColumn("s_duration_sec", col("s_end") - col("s_start"))
+    .withColumn("session_duration_sec", col("session_end") - col("session_start"))
     
 session_df = session_df \
     .withColumn(
         "is_bounce", 
-        when(session_df.unique_events == 1, 1) \
+        when(session_df.unique_event_types == 1, 1) \
         .otherwise(0)
     )
     
 session_df.show()
 
-session_df.selectExpr(
-    "min(s_duration_sec) as min_session_duration",
-    "max(s_duration_sec) as max_session_duration"
-).show()
+# session_df.selectExpr(
+#     "min(s_duration_sec) as min_session_duration",
+#     "max(s_duration_sec) as max_session_duration"
+# ).show()
+
+session_df = session_df \
+    .withColumn("session_date", to_date(from_unixtime("session_start"))) \
+    .withColumn("session_key", concat_ws("-", "user_id", "session_id", "session_date")) \
+    .withColumn("session_start", from_unixtime("session_start")) \
+    .withColumn("session_end", from_unixtime("session_end"))
+
+# Write it to S3
+session_df.write \
+    .mode("append") \
+    .partitionBy("session_date") \
+    .parquet("s3a://ml-user-events/website-sessions/")
+
+# Write it to MySQL
+jdbc_url = "jdbc:mysql://localhost:3306/kafka_demo"
+table = "website_sessions"
+
+properties = {"user": "root", "password": "", "driver": "com.mysql.cj.jdbc.Driver"}
+
+session_df \
+    .repartition(4) \
+    .write \
+    .jdbc(url=jdbc_url, table=table, mode="append", properties=properties)
